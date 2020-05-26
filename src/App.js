08e559062495053138aa05b323d4597efe2fae60
aicayzer <aicayzer@icloud.com>
2020-05-26 23:07:22 +0100

    updating home and adding Map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import SimpleMap from "./pages/Map";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <SimpleMap />
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 class Home extends Component {
-  state = {};
+  state = { journey: "" };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
   render() {
     return (
       <div>
-        <h1>Home</h1>
+        <form noValidate autoComplete="off" onSubmit={this.handleSubmit}>
+          <h1>how many trees is my journey?</h1>
+          <TextField
+            id="outlined-basic"
+            label="journey"
+            variant="outlined"
+            name="journey"
+            onChange={this.handleChange}
+          />
+          <br />
+          <Button variant="contained" color="primary" type="submit">
+            Submit
+          </Button>
+        </form>
       </div>
     );
   }
